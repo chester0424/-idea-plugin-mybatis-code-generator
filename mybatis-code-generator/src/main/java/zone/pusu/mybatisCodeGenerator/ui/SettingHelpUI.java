3f34074f6faca89b2e1ec468b7154df04a1037e5
zhangyao <373934650@qq.com>
2022-09-06 23:47:21 +0800

    添加使用帮助

---
+++ mybatis-code-generator/src/main/java/zone/pusu/mybatisCodeGenerator/ui/SettingHelpUI.java
@@ -0,0 +1,68 @@
+package zone.pusu.mybatisCodeGenerator.ui;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.ui.components.JBScrollPane;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+public class SettingHelpUI implements Configurable {
+    @Override
+    public @NlsContexts.ConfigurableName String getDisplayName() {
+        return "Use Guide";
+    }
+
+    @Override
+    public @Nullable JComponent createComponent() {
+        return initUI();
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+
+    }
+
+    private JComponent initUI() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JEditorPane jEditorPane = new JEditorPane();
+        jEditorPane.setEditable(false);
+        URL url = this.getClass().getResource("/help.html");
+        jEditorPane.setContentType("text/html");
+        jEditorPane.addHyperlinkListener(e -> {
+            if (HyperlinkEvent.EventType.ACTIVATED.equals(e.getEventType())) {
+                Desktop desktop = Desktop.getDesktop();
+                try {
+                    desktop.browse(e.getURL().toURI());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        try {
+            jEditorPane.setPage(url);
+        } catch (IOException e) {
+            jEditorPane.setText("<html>Page not found.</html>");
+        }
+
+        JScrollPane jScrollPane = new JBScrollPane(jEditorPane);
+        jScrollPane.setPreferredSize(new Dimension(540, 400));
+
+        panel.add(jScrollPane);
+
+        return panel;
+    }
+}
